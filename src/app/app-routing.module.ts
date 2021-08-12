2a3d6ea1ff1435fe79f0df74930df9589d733d5b
Dmytro Bilous <bilous.d97@gmail.com>
2021-08-12 16:53:49 +0300

    testtask

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {StopwatchComponent} from './stopwatch.component';
+import {FormComponent} from './form.component';
 
 const routes: Routes = [
-  { path: '**', component: StopwatchComponent}
+  { path: '**', component: FormComponent}
 ];
 
 @NgModule({
